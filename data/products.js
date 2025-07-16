9fc3289ec4dc175fe157ebad3b7a74150c4ad5e6
Nabin <127390250+thenabinbohora@users.noreply.github.com>
2025-07-16 09:20:42 +0000

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -57,6 +57,25 @@ class Clothing extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => { 
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products')
+  xhr.send();
+
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -759,3 +778,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
