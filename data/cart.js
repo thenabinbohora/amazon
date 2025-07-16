541c5eac1ca2090cde0ea05ea8391035d42cee75
Nabin <127390250+thenabinbohora@users.noreply.github.com>
2025-07-16 10:31:14 +0000

    Use Promise to load data from backend

--- data/cart.js
+++ data/cart.js
@@ -67,3 +67,13 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/cart')
+  xhr.send();
+
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,22 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+    
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+    
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
